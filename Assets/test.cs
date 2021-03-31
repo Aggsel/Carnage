b3a261b8a0acb96cecf2cb02c5846193ad9d6aed
Carl Eriksson <carl.erikssonn@hotmail.com>
2021-03-31 14:40:06 +0200

    Push Test

---
+++ Assets/test.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello world!");
+    }
+}
