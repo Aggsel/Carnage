61154b62139afdde1c9c6787efdce79e5d33274b
Alexander Vikberg <a19alevi@student.his.se>
2021-04-09 11:07:30 +0200

    Cleaned up prefabs and fully removed ui-requirement
    Overheat-controller in playerprefab creates no congestion

--- Assets/Scripts/OverheatScript.cs
+++ Assets/Scripts/OverheatScript.cs
@@ -5,15 +5,10 @@ using UnityEngine.UI;
 
 public class OverheatScript : MonoBehaviour
 {
-    //[SerializeField] private Slider slider = null;
     [SerializeField] private GameObject player = null;
     [SerializeField] private WeaponAttributes weaponInstance = null;
-    //[SerializeField] private Gradient gradient = null;
-    //[SerializeField] private Image fill = null;
-
     private float heatValue = 0f;
     private float heatMax = 0f;
-
     private bool recentlyHeated = false;
     private bool overheated = false;
     private float coolingInitializeRemaining = 0f;
@@ -25,13 +20,10 @@ public class OverheatScript : MonoBehaviour
     {
         weaponInstance = player.GetComponent<FiringController>().weaponAttributes;
         heatMax = weaponInstance.heatMaximum;
-        //slider.maxValue = weaponInstance.heatMaximum;
-        //slider.value = 0f;
     }
 
     void Update()
     {
-        //slider.maxValue = weaponInstance.heatMaximum;
         heatMax = weaponInstance.heatMaximum;
         if(recentlyHeated == true)
         {
@@ -43,7 +35,6 @@ public class OverheatScript : MonoBehaviour
         }
         else
         {
-            //slider.value -= weaponInstance.coolingRate * Time.deltaTime;
             heatValue -= weaponInstance.coolingRate * Time.deltaTime;
             heatValue = Mathf.Clamp(heatValue, 0f, heatMax);
 
@@ -63,7 +54,6 @@ public class OverheatScript : MonoBehaviour
     public void Heat(float heatGeneration)
     {
         heatValue += heatGeneration;
-        //fill.color = gradient.Evaluate(slider.normalizedValue);
         if(heatValue >= weaponInstance.heatMaximum)
         {
             incrementedCoolingInitialize = weaponInstance.coolingIntializeTime * 1.75f - weaponInstance.coolingIntializeTime;
