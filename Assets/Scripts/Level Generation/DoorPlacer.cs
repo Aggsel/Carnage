ee051fd7b2cb73625f402f072eae95bcf7887a2f
Axel Nyström Svensson <axel.nystrom-svensson@hotmail.com>
2021-04-16 16:53:55 +0200

    Prefabs for tiles added

--- Assets/Scripts/Level Generation/DoorPlacer.cs
+++ Assets/Scripts/Level Generation/DoorPlacer.cs
@@ -7,15 +7,13 @@ public class DoorPlacer : MonoBehaviour
 {
     [Tooltip("Reference to a door prefab.")]
     [SerializeField] private GameObject doorObject = null;
-    [Tooltip("Reference to a wall prefab.")]
-    [SerializeField] private GameObject wallObject = null;
     private GameObject setObject = null;
 
     public void SetDoor(bool isDoor, RoomManager parentRoom){
         if(setObject != null)
             DestroyImmediate(setObject);
 
-        this.setObject = Instantiate(isDoor ? doorObject : wallObject, transform.position, transform.rotation, transform.parent);
+        this.setObject = Instantiate(doorObject, transform.position, transform.rotation, transform.parent);
 
         Door newDoor = this.setObject.GetComponent<Door>();
         parentRoom.AddDoor(newDoor);
