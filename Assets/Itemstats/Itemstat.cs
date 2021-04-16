6e561c65263f6474bfcdd4dabdda80cbe4d90225
Alexander Vikberg <a19alevi@student.his.se>
2021-04-16 11:25:40 +0200

    Finalized Dev Branch for Carnage Build 0.3
    cleaned up project for dev branch build.

--- Assets/Itemstats/Itemstat.cs
+++ Assets/Itemstats/Itemstat.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Itemstat", menuName = "Itemstat")]
-public class Itemstats : ScriptableObject
+public class Itemstat : ScriptableObject
 {
-
     public string itemName;
     public string itemDescription;
     public string attribute;
     public string secondAttribute;
     public float attributePercentage;
     public float secondAttributePercentage;
-
 }
