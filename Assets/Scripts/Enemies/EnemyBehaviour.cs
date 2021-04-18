087958fbc4232865eaf77f02560d236d86ec9ec9
Axel Nyström Svensson <axel.nystrom-svensson@hotmail.com>
2021-04-18 15:24:42 +0200

    NavMeshCompontents added + prefab modifications
    The NavMeshComponents from Unitys github added to the project.
    Modifications to room component prefabs.
    Clean up of some old placeholder room tile assets.

---
+++ Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : EnemyBase
+{
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private GameObject player;
+    private float health = 10.0f;
+
+    void OnEnable(){
+        player = GameObject.Find("Player");
+    }
+
+    public override void OnShot(){
+        agent.SetDestination(player.transform.position);
+
+        this.health -= 5.0f;
+
+        if(CheckDeathCriteria())
+            Destroy(this.gameObject);
+    }
+
+    private bool CheckDeathCriteria(){
+        return this.health < 0.0f;
+    }
+}
