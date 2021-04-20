c7b9d990b9cbbc689eaf7f9d756e897adb604d7d
Alexander Vikberg <a19alevi@student.his.se>
2021-04-20 11:55:19 +0200

    Added Active buff-items, following structure and groundwork for items.
    Note: Some groundwork or foundation still missing for different item behaviours. 
    
    Push adds 3 scripts, 2 of which are scriptable objects (Active, A_Buff), the third being the CooldownController that the player prefab now uses to maintain it's current Active object and it's usage + cooldowns. Also added Itemholder to gamecontroller prefab to hold items (this will later be used when player needs to interract with an item to pick it up, probably..)

---
+++ Assets/Itemstats/Itemholder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Itemholder : MonoBehaviour
+{
+    public Active[] charActives;    // All actives
+    public Active[] charPassives;    // All passives
+}
