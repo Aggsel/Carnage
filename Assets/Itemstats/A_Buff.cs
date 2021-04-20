6536ff6d7b6a8ac32a6f79405e6e09b9c0b3a21c
Alexander Vikberg <a19alevi@student.his.se>
2021-04-20 11:55:19 +0200

    Added Active buff-items, following structure and groundwork for items.
    Note: Some groundwork or foundation still missing for different item behaviours. 
    
    Push adds 4 scripts, 2 of which are scriptable objects (Active, A_Buff), the third being the CooldownController that the player prefab now uses to maintain it's current Active object and it's usage + cooldowns. Fourth script is a simple itemholder list. Also added Itemholder to gamecontroller prefab to hold items (this will later be used when player needs to interract with an item to pick it up, probably..)

---
+++ Assets/Itemstats/A_Buff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Actives/Buffs")]
+public class A_Buff : Active
+{
+    [Tooltip("The attribute which to buff when using this active.")]
+    [SerializeField] private string attributeToBuff = "firerate";
+    [Tooltip("Percentages to shift the attribute with, a value of 2.5 equals 250% modifier.")]
+    [SerializeField] private float increment = 0f;
+
+
+    private Buff reference = null;
+    private AttributeController ac;
+
+    public override void Initialize(GameObject obj)
+    {
+        ac = obj.GetComponent<AttributeController>();
+    }
+    
+    public override void TriggerActive()
+    {
+        reference = ac.AddBuff(attributeToBuff, increment);
+    }
+
+    public override void DetriggerActive()
+    {
+        if(reference != null)
+        {
+            ac.RemoveBuff(reference);
+        }
+    }
+
+}
---
+++ Assets/Scripts/CooldownController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CooldownController : MonoBehaviour
+{
+    public string abilityButton = "Fire1";
+    public Active active;
+
+    [SerializeField] private GameObject player;
+    private float cooldownDuration;
+    private float readyTime;
+    private float cooldownTimeLeft;
+    private float activeActuationTime;
+    private float activeActuationTimeLeft;
+    private bool activeActuated;
+
+    void Start()
+    {
+        Initialize(active, player);
+    }
+
+    public void Initialize(Active selectedActive, GameObject player)
+    {
+        activeActuated = false;
+        active = selectedActive;
+        if(active != null)
+        {
+            cooldownDuration = active.cooldown;
+            activeActuationTime = active.buffTime;
+            activeActuationTimeLeft = activeActuationTime;
+            cooldownTimeLeft = cooldownDuration;
+            active.Initialize(player);
+        }
+        ActiveReady();
+    }
+
+    void Update()
+    {
+        if (!activeActuated)
+        {
+            bool cdFinished = (Time.time > readyTime);
+            if (cdFinished)
+            {
+                ActiveReady();
+                //Change input to controller-specific action button
+                if (Input.GetKeyDown(KeyCode.E) && active != null)
+                {
+                    Triggered();
+                }
+            }
+            else
+            {
+                Cooldown();
+            }
+        }
+        else
+        {
+            ActuationCooldown();
+        }
+    }
+
+    private void Triggered()
+    {
+        activeActuated = true;
+        activeActuationTimeLeft = activeActuationTime;
+        
+        cooldownTimeLeft = cooldownDuration;
+        active.TriggerActive();
+    }
+
+    private void DeTrigger()
+    {
+        activeActuated = false;
+        activeActuationTimeLeft = activeActuationTime;
+        active.DetriggerActive();
+    }
+
+
+    private void ActiveReady()
+    {
+        //UI stuff probably...
+    }
+
+    private void Cooldown()
+    {
+        cooldownTimeLeft -= Time.deltaTime;
+        float roundedFloat = Mathf.Round(cooldownTimeLeft);
+        //write to UI... probably...
+    }
+
+    private void ActuationCooldown()
+    { 
+        activeActuationTimeLeft -= Time.deltaTime;
+        float roundedFloat = Mathf.Round(activeActuationTimeLeft);
+        if (activeActuationTimeLeft <= 0.0)
+        {
+            DeTrigger();
+            readyTime = cooldownDuration + Time.time;
+        }
+    }
+
+
+
+}
