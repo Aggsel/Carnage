e502d62434e33edd8bf1a012a655711e3844d9f0
Axel Nyström Svensson <axel.nystrom-svensson@hotmail.com>
2021-04-23 11:46:22 +0200

    Lightmap baking onto prefabs
    Some caveats:
    * Lightmaps are restricted to only one lightmap texture per mesh, using multiple lightmaps does not work.
    * Lightmap UVs must be generated when importing new 3d models (only for models that should receive baked lighting of course).
    * The light map baking can generate artifacts on tile edges, this can be mitigated by increasing the lightmap textures resolution and or sampling count.
    
    MeshMerge on room generation will now only merge meshes with the "validMaterial" (assigned on the RoomManager object in the inspector)

--- Assets/Scripts/Level Generation/RoomManager.cs
+++ Assets/Scripts/Level Generation/RoomManager.cs
@@ -27,6 +27,11 @@ public class RoomManager : MonoBehaviour
     [SerializeField] List<EnemySpawnPoint> spawnPoints = new List<EnemySpawnPoint>();
     [SerializeField] WaveHandler waveHandler;
 
+    [Header("Mesh Merging")]
+    [Tooltip(@"When merging the meshes the final combined mesh will just have one material. 
+    The merging process will therefore only combine meshes from objects with this material on them.")]
+    [SerializeField] private Material validMaterial;
+
     void OnEnable(){
         onCombatComplete.AddListener(OnCombatComplete);
     }
@@ -126,7 +131,7 @@ public class RoomManager : MonoBehaviour
         }
     }
 
-    private void MergeMeshes(){
+    public void MergeMeshes(){
         Quaternion oldRot = transform.rotation;
         Vector3 oldPos = transform.position;
 
@@ -142,19 +147,26 @@ public class RoomManager : MonoBehaviour
             if(meshFilters[i].transform == transform)
                 continue;
 
+            MeshRenderer currentMR = meshFilters[i].GetComponent<MeshRenderer>();
+
+            if(currentMR.sharedMaterial == validMaterial)
+                continue;
+
             combiners[i].subMeshIndex = 0;
             combiners[i].mesh = meshFilters[i].sharedMesh;
             combiners[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            combiners[i].lightmapScaleOffset = currentMR.lightmapScaleOffset;
 
             Destroy(meshFilters[i]);
-            Destroy(meshFilters[i].GetComponent<MeshRenderer>());
+            Destroy(currentMR);
         }
 
-        finalMesh.CombineMeshes(combiners);
+        finalMesh.CombineMeshes(combiners, true, true, true);
         MeshFilter meshFilter = this.gameObject.AddComponent<MeshFilter>();
         meshFilter.sharedMesh = finalMesh;
         MeshRenderer meshRenderer = this.gameObject.AddComponent<MeshRenderer>();
-        meshRenderer.material = meshFilters[Random.Range(0,meshFilters.Length)].GetComponent<MeshRenderer>().material;
+        meshRenderer.material = validMaterial;
+        meshRenderer.lightmapIndex = 0;
 
         transform.position = oldPos;
         transform.rotation = oldRot;
