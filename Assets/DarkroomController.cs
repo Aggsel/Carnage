705c5dec9ff2cb1a95570a70582cebabf4849045
Alexander Vikberg <a19alevi@student.his.se>
2021-05-26 16:50:14 +0200

    Start of darkroom
    Start of darkroom

--- Assets/DarkroomController.cs
+++ Assets/DarkroomController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class DarkroomController : MonoBehaviour
 {
+
+    private UIController uic = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uic = GameObject.Find("Game Controller Controller/Canvas").GetComponent<UIController>();
     }
 
     // Update is called once per frame
